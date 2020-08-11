1b74e4285ed3582909943cb97307901f432bf198
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    vayu: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db

--- device.mk
+++ device.mk
@@ -164,7 +164,8 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libstdc++.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    libshim_megvii
 
 PRODUCT_PACKAGES += \
     Snap
