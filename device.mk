09062ac81d4940e3fc6532f7a402678dcfbc7727
dianlujitao <dianlujitao@lineageos.org>
2021-03-02 16:57:26 +0800

    vayu: Build GNSS HAL
    * Hardcode vendor loc HIDL version to 4.0 to enable vendor HIDL when
       building w/o BSP.

--- device.mk
+++ device.mk
@@ -211,6 +211,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@2.1-impl-qti \
+    android.hardware.gnss@2.1-service-qti
+
+PRODUCT_PACKAGES += \
+    libbatching \
+    libgeofencing \
+    libgnss
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
