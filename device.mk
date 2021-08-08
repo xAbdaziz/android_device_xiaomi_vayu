2860cad2331b6bb860ea49db7aee99cc80a98501
xawlw <abdulazizawlw@gmail.com>
2021-08-08 12:13:19 +0200

    vayu: Nuke LiveDisplay
    * We already have KCAL

--- device.mk
+++ device.mk
@@ -282,10 +282,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_msmnile
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
